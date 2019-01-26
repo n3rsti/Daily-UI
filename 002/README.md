bd9644045113db3dd4b78f2b91c18ebc7c216013
n3rsti <krzysiek.witucki55@gmail.com>
2019-01-26 16:52:08 +0100

    created 002 Daily UI folder

---
+++ 002/README.md
@@ -0,0 +1,3 @@
+# 002 Daily UI challenge
+## Credit Card Checkout Page
+Live: 
---
+++ 002/app.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: ".header__checkout",
+    data: {
+        cardType: "",
+    },
+    methods: {
+        visa: function(){
+            this.cardType = "Visa";
+        },
+        mastercard: function(){
+            this.cardType = "Mastercard";
+        }
+    }
+});
